d3573592c56fbc97e16b768584318463d76c259c
Recep Erdoğan <rerdogan@pronist.com.tr>
2023-12-27 15:45:23 +0300

    title changed

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
+            <h3>TEST - Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
